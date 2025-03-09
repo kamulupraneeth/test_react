274a2fe512b2cd0a70f1af33dd298d1aa98d8896
Kamulu Praneeth <kamulupraneeth@gmail.com>
2025-03-09 21:41:50 +0530

    Initial commit

---
+++ src/components/LazyComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { lazy, Suspense } from "react";
+
+// lazy load component
+const About = lazy(() => import("./About"));
+
+const LazyComponent = () => {
+  return (
+    <>
+      <h1>Lazily loading components</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <About />
+      </Suspense>
+    </>
+  );
+};
+
+export default LazyComponent;
