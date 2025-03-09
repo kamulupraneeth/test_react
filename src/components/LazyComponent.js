bf392dcecf58769e602ea4a265407d90a0f83952
Kamulu Praneeth <kamulupraneeth@gmail.com>
2025-03-09 22:27:59 +0530

    text change

--- src/components/LazyComponent.js
+++ src/components/LazyComponent.js
@@ -7,7 +7,7 @@ const About = lazy(() => import("./About"));
 const LazyComponent = () => {
   return (
     <>
-      <h1>Lazily loading components</h1>
+      <h1>Lazily loading components123</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <About />
       </Suspense>
